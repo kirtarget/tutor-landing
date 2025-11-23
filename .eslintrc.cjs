761ea7d8d356af07c04ad76e4a734292b577a244
kirtarget <sheldongriffiths1@gmail.com>
2025-11-23 18:05:35 +0300

    Add ESLint + Prettier setup and VSCode autosave/format on focus change

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // сюда можно добавлять свои правила
+    "prettier/prettier": "warn",
+  },
+};
